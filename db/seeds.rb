64c61c6fbb88b148b6e4a4df26777c09f2fec044
Ginevra Fontes Williams <ginevra.f.williams@gmail.com>
2019-11-27 17:15:56 +0100

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+restaurants_attributes = [
+  {
+    name: 'Al Moro',
+    address: 'via Margutta 59, Rome',
+    phone_number: '3314088031',
+    category: 'italian'
+  },
+  {
+    name: 'Dal Bolognese',
+    address: 'Via Ripetta 23, Rome',
+    phone_number: '3314066031',
+    category: 'italian'
+  },
+  {
+    name: 'Zuma',
+    address: 'via del Corso 320, Rome',
+    phone_number: '3315088031',
+    category: 'japanese'
+  },
+  {
+    name: 'China Tang',
+    address: 'Kings Road 384, London',
+    phone_number: '0704088031',
+    category: 'italian'
+  },
+  {
+    name: 'Poilane',
+    address: 'Duke of York, London',
+    phone_number: '0704008031',
+    category: 'french'
+  }
+]
+Restaurant.create!(restaurants_attributes)
